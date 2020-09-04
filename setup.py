bf65969f3c990615c1bf99d9d86207b7572ba94f
cyf203 <cyfhopkins@gmail.com>
2020-09-04 18:38:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="medpc2excel",
-    version="1.5.5",
+    version="2.0.2",
     license="MIT",
     author="Yifeng Cheng",
     author_email="cyfhopkins@gmail.com",
@@ -14,11 +14,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cyf203/medpc2excel",
     packages=setuptools.find_packages(),
-    install_requires = ['numpy', 'pandas','dill','openpyxl'],
+    install_requires = ['numpy', 'pandas','dill'],
     include_package_data=True,
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-)
